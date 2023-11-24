76c308e3efe005000c9c3390a349a990efb5fcf0
öznur timur <oznur.timur@ozu.edu.tr>
2023-11-24 22:34:31 +0100

    slider widget for home page

---
+++ movie_tracker/lib/widgets/trending_slider.dart
@@ -0,0 +1,58 @@
+import 'package:movie_tracker/screens/movie_details.dart';
+import 'package:movie_tracker/shared/constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class TrendingSlider extends StatelessWidget {
+  const TrendingSlider({
+    Key? key,
+    required this.snapshot,
+  }) : super(key: key);
+
+  final AsyncSnapshot snapshot;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: CarouselSlider.builder(
+        itemCount: 10,
+        options: CarouselOptions(
+          height: 300,
+          autoPlay: true,
+          viewportFraction: 0.55,
+          enlargeCenterPage: true,
+          pageSnapping: true,
+          autoPlayCurve: Curves.easeIn,
+          autoPlayAnimationDuration: const Duration(seconds: 1),
+        ),
+        itemBuilder: (context, itemIndex, pageViewIndex) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailsScreen(
+                    media: snapshot.data[itemIndex],
+                  ),
+                ),
+              );
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: SizedBox(
+                height: 300,
+                width: 200,
+                child: Image.network(
+                  '${Constants.imagePath}${snapshot.data[itemIndex].posterPath}',
+                  filterQuality: FilterQuality.high,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
