658ec24daf4a11aac9d533d80a344d992d4fab6f
öznur timur <oznur.timur@ozu.edu.tr>
2023-11-24 22:45:20 +0100

    colors added to shared folder

--- movie_tracker/lib/shared/colors.dart
+++ movie_tracker/lib/shared/colors.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class Colours {
   static const scaffoldBgColor = Color(0xFF23272E);
   static const ratingColor = Color(0xFFFFC107);
+  static const light_g=Color.fromARGB(255, 113, 189, 113);
+  static const dark_g=Color.fromARGB(255, 5, 67, 27);
 }
