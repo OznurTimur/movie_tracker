721f59fc2393007bffca9bda9dbb9df1008e21f1
öznur timur <oznur.timur@ozu.edu.tr>
2023-11-29 23:52:19 +0100

    user profile page

--- movie_tracker/lib/screens/user_profile_screen.dart
+++ movie_tracker/lib/screens/user_profile_screen.dart
@@ -1,85 +1,140 @@
+// User name
+// Switch between dark light theme
+// Switch between country / language
+// User can select / unselect services the user is subscribed (i.e. Netflix, etc.)
+
 import 'package:flutter/material.dart';
+import 'package:movie_tracker/api/api.dart';
 
 class UserProfileScreen extends StatefulWidget {
-  const UserProfileScreen({super.key});
-
   @override
   _UserProfileScreenState createState() => _UserProfileScreenState();
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
-  bool isDarkTheme = false; // Placeholder for dark theme toggle
-  bool isSubscribedToNetflix = false; // Placeholder for Netflix subscription
-  // Add more subscription bools for other services
-  String selectedLanguage = 'English'; // Default selected language
+  Map<String, dynamic>? accountData;
+  Api api = Api();
+  bool isDarkTheme = false;
+  String selectedCountryLanguage = 'English';
+  List<String> subscribedServices =
+      []; // Initialize an empty list for subscribed services
+  List<String> availableServices = [
+    'Netflix',
+    'Amazon Prime',
+    'Disney+'
+  ]; // List of available services
+
+  void toggleSubscription(String service) {
+    setState(() {
+      if (subscribedServices.contains(service)) {
+        subscribedServices
+            .remove(service); // Unselect service if already subscribed
+      } else {
+        subscribedServices.add(service); // Select service if not subscribed
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAccountDetails();
+  }
+
+  Future<void> fetchAccountDetails() async {
+    final data = await api.fetchAccountDetails();
+    setState(() {
+      accountData = data;
+    });
+  }
+
+  void toggleTheme(bool value) {
+    setState(() {
+      isDarkTheme = value;
+    });
+  }
+
+  void changeLanguage(String language) {
+    setState(() {
+      selectedCountryLanguage = language;
+    });
+    // Implement logic to change the language/locale as needed
+    // You might use packages like intl or flutter_localizations for this purpose
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User Profile'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'User Name', // Display user name here
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Theme', // Theme selection
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            SwitchListTile(
-              title: Text('Dark Mode'),
-              value: isDarkTheme,
-              onChanged: (value) {
-                setState(() {
-                  isDarkTheme = value;
-                  // Implement theme switching logic here
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            DropdownButton<String>(
-              value: selectedLanguage,
-              onChanged: (newValue) {
-                setState(() {
-                  selectedLanguage = newValue!;
-                  // Implement logic to change the language
-                });
-              },
-              items: <String>['English', 'Turkish', 'Spanish']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            // Implement language/country selection widgets here
-            SizedBox(height: 20),
-            Text(
-              'Subscription',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            CheckboxListTile(
-              title: Text('Netflix'), // Example subscription
-              value: isSubscribedToNetflix,
-              onChanged: (value) {
-                setState(() {
-                  isSubscribedToNetflix = value!;
-                  // Implement subscription logic here
-                });
-              },
-            ),
-            // Add more CheckboxListTile widgets for other services
-          ],
+    return MaterialApp(
+      theme: isDarkTheme ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('User Profile'),
+          ),
+          body: Center(
+          child: accountData == null
+              ? CircularProgressIndicator()
+              : Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text(
+                    'Account Details:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  Text('Username: ${accountData!['username']}'),
+                  Text('ID: ${accountData!['id']}'),
+                  SizedBox(height: 20),
+                  Switch(
+                    value: isDarkTheme,
+                    onChanged: toggleTheme,
+                    activeColor: Colors.greenAccent,
+                    inactiveTrackColor:
+                        const Color.fromARGB(255, 113, 110, 110),
+                  ),
+                  SizedBox(height: 20),
+                  DropdownButton<String>(
+                    value: selectedCountryLanguage,
+                    onChanged: (String? newValue) {
+                      if (newValue != null) {
+                        changeLanguage(newValue);
+                      }
+                    },
+                    items: <String>[
+                      'English',
+                      'French',
+                      'Spanish',
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Subscribed Services:',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Wrap(
+                    alignment: WrapAlignment.center,
+                    spacing: 8.0,
+                    children: availableServices.map((service) {
+                      bool isSelected = subscribedServices.contains(service);
+                      return FilterChip(
+                        label: Text(service),
+                        selected: isSelected,
+                        onSelected: (isSelected) {
+                          toggleSubscription(service);
+                        },
+                        selectedColor: Colors.green,
+                        backgroundColor: Colors.grey[300],
+                        checkmarkColor: Colors.white,
+                      );
+                    }).toList(),
+                  ),
+                ]),
         ),
       ),
+      
     );
   }
 }
