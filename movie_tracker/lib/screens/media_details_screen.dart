c3fd19d08eb395af32192bc4d5ef357ede560e43
öznur timur <oznur.timur@ozu.edu.tr>
2023-12-06 00:55:48 +0100

    name correction

--- movie_tracker/lib/screens/media_details_screen.dart
+++ movie_tracker/lib/screens/media_details_screen.dart
@@ -6,7 +6,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_tracker/shared/constants.dart';
 import 'package:movie_tracker/widgets/back_button.dart';
 import 'package:movie_tracker/api/api.dart';
-import 'package:movie_tracker/screens/watchlist_screen.dart';
 import 'package:movie_tracker/screens/actor_details.dart';
 import 'package:movie_tracker/models/Actor.dart';
 import 'package:movie_tracker/shared/colors.dart';
@@ -29,10 +28,8 @@ class _DetailsScreenState extends State<DetailsScreen> {
   double? userRating = 0; // To store the user's rating
   Api api = Api(); // Instance of Api class
   late Media media;
-  Watchlists watchlists = Watchlists(); // Instance of Watchlists
   List<Actor> actors = [];
   Map<int, String> genresMap = {};
- 
 
   @override
   void initState() {
@@ -41,18 +38,6 @@ class _DetailsScreenState extends State<DetailsScreen> {
     fetchMovieCredits();
   }
 
-  void addToWatchlist() async {
-  try {
-    
-    final sessionId = await api.createSession();
-    // Replace id and media_type with appropriate values
-    await api.addToWatchlist(media.id, media.mediaType, sessionId);
-    // Optionally, you can add logic here upon successful addition to watchlist
-  } catch (e) {
-    // Handle errors or exceptions here
-    print('Error adding to watchlist: $e');
-  }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -149,204 +134,121 @@ class _DetailsScreenState extends State<DetailsScreen> {
                       ],
                     ),
                   ),
-                  SizedBox(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Row(
-                            children: [
-                              Text(
-                                'Rate out of 10: ',
-                                style: GoogleFonts.roboto(
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
-                              SizedBox(width: 10),
-                              Container(
-                                width: 60,
-                                child: TextField(
-                                  keyboardType: TextInputType.number,
-                                  onChanged: (value) {
-                                    setState(() {
-                                      userRating = double.tryParse(value);
-                                    });
-                                  },
-                                ),
-                              ),
-                              SizedBox(width: 10),
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Rate',
-                                  style: GoogleFonts.roboto(
-                                    fontSize: 17,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.amber,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 16),
                 ],
               ),
             ),
           ),
-          SliverToBoxAdapter(
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 16),
-                child: Row(
-                  children: [
-                    Text(
-                      'Add to Watchlist',
-                      style: GoogleFonts.openSans(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(width: 8), // Adjust the spacing as needed
-                    FloatingActionButton(
-                      onPressed: () {
-                       addToWatchlist();
-                      },
-                      child: Icon(Icons.add),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Genre: ',
-                      style: GoogleFonts.roboto(
-                          fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    //genre text here
-                  ],
-                )),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Director: ',
-                      style: GoogleFonts.roboto(
-                          fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    //director text here
-                  ],
-                )),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Duration: ',
-                      style: GoogleFonts.roboto(
-                          fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    //duration text here
-                    FutureBuilder<Media>(
-                      future: api.fetchMediaDetails(
-                          media.id,
-                          media.mediaType), // Use method to fetch the media details
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return CircularProgressIndicator(); // Show a loading indicator while fetching data
-                        } else if (snapshot.hasError) {
-                          return Text(
-                              'Error: ${snapshot.error}'); // Handle error state
-                        } else if (!snapshot.hasData) {
-                          return Text(
-                              'No data available'); // Handle no data state
-                        } else {
-                          // Access the runtime from the fetched media details
-                          final String duration =
-                              '${snapshot.data!.runTime} minutes';
-                          return Text(
-                            duration,
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.black,
-                            ),
-                          );
-                        }
-                      },
-                    ),
-                  ],
-                )),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Year of release: ',
-                      style: GoogleFonts.roboto(
-                          fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    //year text here
-                  ],
-                )),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-                padding: const EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'PEGI info: ',
-                      style: GoogleFonts.roboto(
-                          fontSize: 17, fontWeight: FontWeight.bold),
-                    ),
-                    //pegi text here
-                  ],
-                )),
-          ),
+      
+          // SliverToBoxAdapter(
+          //   child: Container(
+          //       padding: const EdgeInsets.all(8),
+          //       decoration: BoxDecoration(
+          //         border: Border.all(color: Colors.grey),
+          //         borderRadius: BorderRadius.circular(10),
+          //       ),
+          //       child: Row(
+          //         children: [
+          //           Text(
+          //             'Genre: ',
+          //             style: GoogleFonts.roboto(
+          //                 fontSize: 17, fontWeight: FontWeight.bold),
+          //           ),
+          //           //genre text here
+          //         ],
+          //       )),
+          // ),
+          // SliverToBoxAdapter(
+          //   child: Container(
+          //       padding: const EdgeInsets.all(8),
+          //       decoration: BoxDecoration(
+          //         border: Border.all(color: Colors.grey),
+          //         borderRadius: BorderRadius.circular(10),
+          //       ),
+          //       child: Row(
+          //         children: [
+          //           Text(
+          //             'Director: ',
+          //             style: GoogleFonts.roboto(
+          //                 fontSize: 17, fontWeight: FontWeight.bold),
+          //           ),
+          //           //director text here
+          //         ],
+          //       )),
+          // ),
+          // SliverToBoxAdapter(
+          //   child: FutureBuilder<Media>(
+          //     future: api.fetchMediaDetails(media.id,
+          //         media.mediaType), // Replace with your media ID and type
+          //     builder: (context, snapshot) {
+          //       if (snapshot.connectionState == ConnectionState.waiting) {
+          //         return CircularProgressIndicator(); // or a loading indicator
+          //       } else if (snapshot.hasError) {
+          //         return Text('Error loading media details: ${snapshot.error}');
+          //       } else if (!snapshot.hasData) {
+          //         return Text('No data available.');
+          //       } else {
+          //         Media media123 = snapshot.data!;
+
+          //         return Row(
+          //           children: [
+          //             Text(
+          //               'Duration:  ',
+          //               style: GoogleFonts.roboto(
+          //                 fontSize: 17,
+          //                 fontWeight: FontWeight.bold,
+          //               ),
+          //             ),
+          //             SizedBox(
+          //               width: 5,
+          //             ),
+          //             Text(
+          //               '${media123.runTime} minutes', // Access runTime from fetched media
+          //               style: GoogleFonts.roboto(
+          //                 fontSize: 17,
+          //                 fontWeight: FontWeight.normal,
+          //               ),
+          //             ),
+          //           ],
+          //         );
+          //       }
+          //     },
+          //   ),
+          // ),
+          // SliverToBoxAdapter(
+          //   child: Container(
+          //       padding: const EdgeInsets.all(8),
+          //       decoration: BoxDecoration(
+          //         border: Border.all(color: Colors.grey),
+          //         borderRadius: BorderRadius.circular(10),
+          //       ),
+          //       child: Row(
+          //         children: [
+          //           Text(
+          //             'Year of release: ',
+          //             style: GoogleFonts.roboto(
+          //                 fontSize: 17, fontWeight: FontWeight.bold),
+          //           ),
+          //         ],
+          //       )),
+          // ),
+          // SliverToBoxAdapter(
+          //   child: Container(
+          //       padding: const EdgeInsets.all(8),
+          //       decoration: BoxDecoration(
+          //         border: Border.all(color: Colors.grey),
+          //         borderRadius: BorderRadius.circular(10),
+          //       ),
+          //       child: Row(
+          //         children: [
+          //           Text(
+          //             'PEGI info: ',
+          //             style: GoogleFonts.roboto(
+          //                 fontSize: 17, fontWeight: FontWeight.bold),
+          //           ),
+          //           //pegi text here
+          //         ],
+          //       )),
+          // ),
           SliverToBoxAdapter(
             child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 16),
