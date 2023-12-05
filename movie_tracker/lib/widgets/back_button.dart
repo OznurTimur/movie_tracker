fd717feaf0dbfe34a6e841482f2eaca36331c084
öznur timur <oznur.timur@ozu.edu.tr>
2023-12-06 00:22:10 +0100

    back button modified

--- movie_tracker/lib/widgets/back_button.dart
+++ movie_tracker/lib/widgets/back_button.dart
@@ -1,37 +1,13 @@
-import 'package:movie_tracker/shared/colors.dart';
 import 'package:flutter/material.dart';
 
-class BackBtn extends StatelessWidget{
-
-@override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar.large(
-            leading: Container(
-              height:70,
-              width:70,
-              margin: const EdgeInsets.only (
-                top:16,
-                left:16,
-              ),
-              decoration: BoxDecoration(
-                color: Colours.scaffoldBgColor,
-                borderRadius: BorderRadius.circular(8),
-                ),
-                child: IconButton(
-                  onPressed: (){
-                     Navigator.pop(context);
-                  },
-                  icon:const Icon (
-                    Icons.arrow_back_rounded,
-                  )
-                )
-            ),
-          ),
-        ]
-      ),
+class BackBtn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(Icons.arrow_back),
     );
   }
 }
