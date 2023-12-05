7a9a7d227554de343b18a6352e5eb62e0ae1448a
öznur timur <oznur.timur@ozu.edu.tr>
2023-12-06 00:59:14 +0100

    watchlist screen

--- movie_tracker/lib/screens/watchlist_screen.dart
+++ movie_tracker/lib/screens/watchlist_screen.dart
@@ -1,47 +1,108 @@
-// My watchlist
-// Movies / TV series that you want to see
-// You should include the poster for each item
-
 import 'package:flutter/material.dart';
-import 'package:movie_tracker/models/Media.dart';
+import 'package:movie_tracker/models/Movie.dart';
+import 'package:movie_tracker/screens/media_details_screen.dart';
 import 'package:movie_tracker/api/api.dart';
+import 'package:movie_tracker/shared/constants.dart';
+import 'package:movie_tracker/widgets/back_button.dart';
 
-// ignore: must_be_immutable
-class Watchlists extends StatelessWidget {
+class Watchlists extends StatefulWidget {
+  @override
+  _WatchlistsState createState() => _WatchlistsState();
+}
+
+class _WatchlistsState extends State<Watchlists> {
   final Api api = Api();
-  List<Media> userWatchlist = [];
+  List<Map<String, dynamic>>TVlist= [];
+  List<Map<String, dynamic>>MovieList = [];
 
-  Watchlists({super.key}); 
+  @override
+  void initState() {
+    super.initState();
+    fetchWatchlistedMovies();
+    fetchWatchlistedTVShows();
 
-  void addToWatchlist(Media movie) {
-    // Add the movie to the user's watchlist
-    userWatchlist.add(movie);
   }
 
- 
-  
- @override
-  Widget build(BuildContext context) {
+    Future<void> fetchWatchlistedMovies() async {
+    final Map<String, dynamic>? data = await api.fetchWatchlistedMovies();
+    if (data != null) {
+      setState(() {
+        MovieList = List<Map<String, dynamic>>.from(data['results']);
+      });
+    }
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Watchlist'),
-      ),
-      body: ListView.builder(
-        
-        itemCount: userWatchlist.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            //leading: Image.network(userWatchlist[index].posterUrl),
-            title: Text(userWatchlist[index].title),
-            
-          );
-        },
+   Future<void> fetchWatchlistedTVShows() async {
+    final Map<String, dynamic>? data = await api.fetchWatchlistedTVShows();
+    if(data != null){
+      setState(() {
+        TVlist = List<Map<String, dynamic>>.from(data['results']);
+      });
+    }
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+   return DefaultTabController(
+      length: 2, // Number of tabs (Movies and TV Shows)
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Watchlists'),
+          leading: BackBtn(),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'TV Shows'),
+              Tab(text: 'Movies'),
+            ],
+          ),
+        ),
+      body: TabBarView(
+        children: [
+          _buildWatchlist(TVlist),
+          _buildWatchlist(MovieList),
+        ],
+        ),
       ),
     );
-   
-         
-      
-    }
+  }
 
-}
+Widget _buildWatchlist(List<Map<String, dynamic>> userWatchlist) {
+    return userWatchlist.isEmpty
+        ? Center(child: CircularProgressIndicator())
+        : ListView.builder(
+            itemCount: userWatchlist.length,
+            itemBuilder: (context, index) {
+              String posterPath = userWatchlist[index]['poster_path'];
+               String name = userWatchlist[index]['media_type'] == 'movie'
+                ? userWatchlist[index]['title']
+                : userWatchlist[index]['name'];
+
+             return GestureDetector(
+              onTap: () {
+                // Navigate to movie or TV show details screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsScreen(
+                      media: Movie.fromJson(userWatchlist[index]),
+                    ),
+                  ),
+                );
+              },
+              child: ListTile(
+                leading: Image.network(
+                  'https://image.tmdb.org/t/p/w200/$posterPath',
+                  width: 50,
+                  height: 50,
+                ),
+                title: Text(name),
+               
+              ),
+            );
+          },
+        );
+     }
+  }
+
+
